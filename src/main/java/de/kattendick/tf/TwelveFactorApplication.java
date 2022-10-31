1a9187824991ebeabe684860b9a5c8b04cf6cda8
LeonKattendick <leon.kattendick@gmail.com>
2022-10-31 23:33:38 +0100

    startup

---
+++ src/main/java/de/kattendick/tf/TwelveFactorApplication.java
@@ -0,0 +1,12 @@
+package de.kattendick.tf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwelveFactorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwelveFactorApplication.class, args);
+    }
+}
